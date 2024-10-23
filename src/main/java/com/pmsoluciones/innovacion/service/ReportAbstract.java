40e843e1a1dc8b8d73c7b0c2572de02c60eb3fef
henoch <henoch.cruz@mail>
2024-10-23 10:49:36 -0600

    cambios

--- src/main/java/com/pmsoluciones/innovacion/service/ReportAbstract.java
+++ src/main/java/com/pmsoluciones/innovacion/service/ReportAbstract.java
@@ -6,105 +6,123 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import jakarta.servlet.http.HttpServletResponse;
 
-public  abstract class ReportAbstract {
+public abstract class ReportAbstract {
 	public XSSFWorkbook workbook;
-    public XSSFSheet sheet;
+	public XSSFSheet sheet;
 
-    public void newReportExcel() {
-        workbook = new XSSFWorkbook();
-    }
+	public void newReportExcel() {
+		workbook = new XSSFWorkbook();
+	}
 
-    public HttpServletResponse initResponseForExportExcel(HttpServletResponse response, String fileName) {
-        response.setContentType("application/octet-stream");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
+	public HttpServletResponse initResponseForExportExcel(HttpServletResponse response, String fileName) {
+		response.setContentType("application/octet-stream");
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+		String currentDateTime = dateFormatter.format(new Date());
 
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=" + fileName + "_" + currentDateTime + ".xlsx";
-        response.setHeader(headerKey, headerValue);
-        return response;
-    }
-    public void writeTableHeaderExcel( List<String> headers,Integer rowPos, Integer mergeCols) {
-    	this.writeTableHeaderExcel( headers,rowPos, mergeCols ,true);
-    }
-    
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=" + fileName + "_" + currentDateTime + ".xlsx";
+		response.setHeader(headerKey, headerValue);
+		return response;
+	}
+
+	public void writeTableHeaderExcel(List<String> headers, Integer rowPos, Integer mergeCols) {
+		this.writeTableHeaderExcel(headers, rowPos, mergeCols, true);
+	}
+
 //    GENERA LOS HEADER
-    public void writeTableHeaderExcel(List<String> headers,Integer posRow, Integer mergeCols ,boolean isMerge) {
+	public void writeTableHeaderExcel(List<String> headers, Integer posRow, Integer mergeCols, boolean isMerge) {
 
-        // sheet
+		// sheet
 //        sheet = workbook.createSheet(sheetName);
-        Row row = sheet.createRow(posRow);
-		
-        CellStyle style = workbook.createCellStyle();
-        XSSFFont font = workbook.createFont();
-        font.setBold(true);
-       
-        style.setFont(font);
-        style.setAlignment(HorizontalAlignment.CENTER);
-                
-        Integer celPos = 0;
-        
-        
-        if(headers.size() == 1) {
-        	createCell(row, celPos, headers.get(0), style);
-        	if(isMerge) {
-        		sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, celPos, celPos + mergeCols));
-            	celPos = celPos + mergeCols+1;
-        	}
-        }else {
-        	for (String tituloH : headers) {
-            	createCell(row, celPos, tituloH, style);
-            	
-            	if(celPos > 0 && isMerge) {
-            		sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, celPos, celPos + mergeCols));
-                	celPos = celPos + mergeCols+1;
-            	}else {
-            		celPos +=1;
-            	}
-            	
-    		}
-        }
-        
-        
-      
-    }
+		Row row = sheet.createRow(posRow);
 
-    public void createCell(org.apache.poi.ss.usermodel.Row row, int columnCount, Object value, CellStyle style) {
-        sheet.autoSizeColumn(columnCount);
-        Cell cell = row.createCell(columnCount);
-        if (value instanceof Integer) {
-            cell.setCellValue((Integer) value);
-        } else if (value instanceof Double) {
-            cell.setCellValue((Double) value);
-        } else if (value instanceof Boolean) {
-            cell.setCellValue((Boolean) value);
-        } else if (value instanceof Long) {
-            cell.setCellValue((Long) value);
-        }else if (value instanceof BigDecimal){
-            cell.setCellValue( (String) value.toString());
-        } 
-        else {
-            cell.setCellValue((String) value);
-        }
-        cell.setCellStyle(style);
-    }
+		CellStyle style = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
 
-    public CellStyle getFontContentExcel() {
-        CellStyle style = workbook.createCellStyle();
-        XSSFFont font = workbook.createFont();
-        font.setFontHeight(14);
-        style.setFont(font);
-        return style;
-    }
+		style.setFont(font);
+		style.setAlignment(HorizontalAlignment.CENTER);
+
+		style.setBorderBottom(BorderStyle.THIN);
+		style.setBorderLeft(BorderStyle.THIN);
+		style.setBorderRight(BorderStyle.THIN);
+		style.setBorderTop(BorderStyle.THIN);
+
+		Integer celPos = 0;
+
+		if (headers.size() == 1) {
+			createCell(row, celPos, headers.get(0), style);
+			if (isMerge) {
+				CellRangeAddress region = new CellRangeAddress(posRow, posRow, celPos, celPos + mergeCols);
+        		RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+        		RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+        		RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+        		RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+				sheet.addMergedRegion(region);
+
+
+				celPos = celPos + mergeCols + 1;
+			}
+		} else {
+			for (String tituloH : headers) {
+				createCell(row, celPos, tituloH, style);
+
+				if (celPos > 0 && isMerge) {					
+					CellRangeAddress region = new CellRangeAddress(posRow, posRow, celPos, celPos + mergeCols);
+					sheet.addMergedRegion(region);
+					RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+	        		RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+	        		RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+	        		RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+					celPos = celPos + mergeCols + 1;
+				} else {
+					celPos += 1;
+				}
+
+			}
+		}
+
+	}
+
+	public Cell createCell(org.apache.poi.ss.usermodel.Row row, int columnCount, Object value, CellStyle style) {
+		sheet.autoSizeColumn(columnCount);
+		Cell cell = row.createCell(columnCount);
+		if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		} else if (value instanceof Double) {
+			cell.setCellValue((Double) value);
+		} else if (value instanceof Boolean) {
+			cell.setCellValue((Boolean) value);
+		} else if (value instanceof Long) {
+			cell.setCellValue((Long) value);
+		} else if (value instanceof BigDecimal) {
+			cell.setCellValue((String) value.toString());
+		} else {
+			cell.setCellValue((String) value);
+		}
+		 cell.setCellStyle(style);
+		 
+		 return cell;
+	}
+
+	public CellStyle getFontContentExcel() {
+		CellStyle style = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setFontHeight(14);
+		style.setFont(font);
+		return style;
+	}
 }
--- src/main/java/com/pmsoluciones/innovacion/service/impl/IndicadoresExcelReport.java
+++ src/main/java/com/pmsoluciones/innovacion/service/impl/IndicadoresExcelReport.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
@@ -26,6 +29,8 @@ import com.pmsoluciones.innovacion.service.ReportAbstract;
 
 @Service
 public class IndicadoresExcelReport extends ReportAbstract {
+	
+	String[] MESES = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
 
 	public ResponseEntity<ByteArrayResource> exportToExcel(
 			ReporteInformeOperativoRequest reporteInformeOperativoRequest) throws Exception {
@@ -58,6 +63,7 @@ public class IndicadoresExcelReport extends ReportAbstract {
 		posRow = listadoPpb(sheep, posRow, proyecto.getLstPpb());
 		posRow += 1;
 		posRow = listadoAcr(sheep, posRow, proyecto.getLstAcr());
+
 	}
 
 	Integer listadoMetricas(XSSFSheet sheep, Integer posRow, ProyectoDto proyecto,
@@ -75,7 +81,7 @@ public class IndicadoresExcelReport extends ReportAbstract {
 				reporteInformeOperativoRequest.getRefUrlRepos());
 		writeTableHeaderExcel(headers2, posRow, mergeCols);
 
-		posRow += 1;
+		posRow += 2;
 
 		// gerenar listado de meses
 		List<String> headers3 = new ArrayList<String>();
@@ -95,24 +101,32 @@ public class IndicadoresExcelReport extends ReportAbstract {
 			Row row = sheet.createRow(posRow);
 			int columnCount = 0;
 
-			CellStyle style = getFontContentExcel();
+			CellStyle style = workbook.createCellStyle();
+			style.setAlignment(HorizontalAlignment.LEFT);
+			style.setBorderBottom(BorderStyle.THIN);
+			style.setBorderLeft(BorderStyle.THIN);
+			style.setBorderRight(BorderStyle.THIN);
+			style.setBorderTop(BorderStyle.THIN);
 
 			createCell(row, columnCount++, metrica.getNomMetrica(), style);
-			
+
 			for (String fecha : reporteInformeOperativoRequest.getLstFechas()) {
-				
-				if(metrica.getFechasProgramadas() != null){
+
+				Cell cell = createCell(row, columnCount, "", style);
+
+				if (metrica.getFechasProgramadas() != null) {
 					for (FechaProgramadaDto fechProg : metrica.getFechasProgramadas()) {
-						if(fecha.equals(fechProg.getFecProgramada())) {		
-							
-							createCell(row, columnCount, fechProg.getNumResultMetrica(), style);
+						if (fecha.equals(fechProg.getFecProgramada())) {
+//							createCell(row, columnCount, fechProg.getNumResultMetrica(), style);
+							if (null != fechProg.getNumResultMetrica()) {
+								cell.setCellValue((String) fechProg.getNumResultMetrica().toString());
+							}
+
 						}
 					}
 				}
-				
-							
-					
-		    columnCount++;
+
+				columnCount++;
 			}
 
 			posRow += 1;
@@ -137,6 +151,11 @@ public class IndicadoresExcelReport extends ReportAbstract {
 			posRow += 1;
 			CellStyle style = workbook.createCellStyle();
 			style.setAlignment(HorizontalAlignment.LEFT);
+			style.setBorderBottom(BorderStyle.THIN);
+			style.setBorderLeft(BorderStyle.THIN);
+			style.setBorderRight(BorderStyle.THIN);
+			style.setBorderTop(BorderStyle.THIN);
+
 			for (MetricasDto metrica : lstMetricas) {
 
 				Row row = sheet.createRow(posRow);
@@ -145,13 +164,32 @@ public class IndicadoresExcelReport extends ReportAbstract {
 				createCell(row, columnCount, metrica.getFechaDoc(), style);
 				columnCount += 1;
 				createCell(row, columnCount, metrica.getNomDoc(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				CellRangeAddress region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getNomDoc(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getDesUrl(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 
 				posRow += 1;
@@ -177,19 +215,43 @@ public class IndicadoresExcelReport extends ReportAbstract {
 				Row row = sheet.createRow(posRow);
 				int columnCount = 0;
 
-				CellStyle style = getFontContentExcel();
+				CellStyle style = workbook.createCellStyle();
+				style.setAlignment(HorizontalAlignment.LEFT);
+				style.setAlignment(HorizontalAlignment.LEFT);
+				style.setBorderBottom(BorderStyle.THIN);
+				style.setBorderLeft(BorderStyle.THIN);
+				style.setBorderRight(BorderStyle.THIN);
+				style.setBorderTop(BorderStyle.THIN);
 
 				createCell(row, columnCount, "Linea base de desempeño", style);
 
 				columnCount += 1;
 				createCell(row, columnCount, metrica.getDesLcl(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				CellRangeAddress region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getDesMedia(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getDesUcl(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
 
 				posRow += 1;
 				row = sheet.createRow(posRow);
@@ -197,13 +259,31 @@ public class IndicadoresExcelReport extends ReportAbstract {
 				createCell(row, columnCount, "Desempeño del proyecto", style);
 				columnCount += 1;
 				createCell(row, columnCount, metrica.getDesLclIn(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getDesMediaIn(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 				createCell(row, columnCount, metrica.getDesUclIn(), style);
-				sheet.addMergedRegion(new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols));
+				region = new CellRangeAddress(posRow, posRow, columnCount, columnCount + mergeCols);
+				sheet.addMergedRegion(region);
+				RegionUtil.setBorderTop(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderLeft(BorderStyle.THIN, region, sheet);
+				RegionUtil.setBorderRight(BorderStyle.THIN, region, sheet);
+
 				columnCount = columnCount + mergeCols + 1;
 
 				posRow += 1;
